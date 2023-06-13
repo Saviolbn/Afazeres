f729512d28ba02fdcf05ae07352ad94bf00db56a
Saviolbn <125612304+Saviolbn@users.noreply.github.com>
2023-06-12 22:08:38 -0300

    adicionado toggle

--- jquery.js
+++ jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $("li").on("click",function(){
-        $(this).find(".texto").css("text-decoration","line-through")
-    });
+        $(this).find(".texto").toggleClass("sublinhado")
+    })
 });
