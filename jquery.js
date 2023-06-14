95d62a83f894e842c12b993bb11dfa9a7e130bdc
Saviolbn <125612304+Saviolbn@users.noreply.github.com>
2023-06-14 15:04:47 -0300

    adicionado função de adicionar tarefa

--- jquery.js
+++ jquery.js
@@ -15,5 +15,29 @@ $(document).ready(function () {
         const $eleLista = ($(this).parent().parent());
         $eleLista.next().after($eleLista);
     });
-    
+    function criarTarefa(){
+        var linha = document.createElement("li");
+        var lixeira = document.createElement("span");
+        lixeira.className = "material-icons delete"
+        lixeira.innerHTML = "delete";
+        var campo = document.createElement("span");
+        campo.className = "texto"
+        campo.innerHTML = "teste";
+        var lapis = document.createElement("span");
+        lapis.className = "material-icons edit"
+        lapis.innerHTML = "edit"
+        var baixo = document.createElement("span");
+        baixo.className = "material-icons down"
+        baixo.innerHTML = "expand_more"
+        var cima = document.createElement("span");
+        cima.className = "material-icons up"
+        cima.innerHTML = "expand_less"
+        var direita = document.createElement("div");
+        $(direita).append(lapis,baixo,cima);
+        $(linha).append(lixeira,campo,direita);
+        $("ul").append(linha);
+    }
+    $("button").on("click",function(){
+        criarTarefa();
+    });
 });
