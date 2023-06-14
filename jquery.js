c62fe48fb7e6fb128e25394821c2a76fd3a3f243
Saviolbn <125612304+Saviolbn@users.noreply.github.com>
2023-06-14 17:53:56 -0300

    adicionado mockup do layout para adição de tarefas

--- jquery.js
+++ jquery.js
@@ -1,43 +1,47 @@
-$(document).ready(function () {
-    $(".texto").on("click",function(){
-        $(this).toggleClass("sublinhado")
-    });
-
-    $(".delete").on("click",function(){
-        $(this).parent().remove()
-    });
-
-    $(".up").on("click",function(){
-        const $eleLista = ($(this).parent().parent());
-        $eleLista.prev().before($eleLista);
-    });
-    $(".down").on("click",function(){
-        const $eleLista = ($(this).parent().parent());
-        $eleLista.next().after($eleLista);
-    });
-    function criarTarefa(){
+function criarTarefa(){
         var linha = document.createElement("li");
         var lixeira = document.createElement("span");
-        lixeira.className = "material-icons delete"
-        lixeira.innerHTML = "delete";
+            lixeira.className = "material-icons delete"
+            lixeira.innerHTML = "delete";
         var campo = document.createElement("span");
-        campo.className = "texto"
-        campo.innerHTML = "teste";
+            campo.className = "texto"
+            campo.innerHTML = "teste";
         var lapis = document.createElement("span");
-        lapis.className = "material-icons edit"
-        lapis.innerHTML = "edit"
+            lapis.className = "material-icons edit"
+            lapis.innerHTML = "edit"
         var baixo = document.createElement("span");
-        baixo.className = "material-icons down"
-        baixo.innerHTML = "expand_more"
+            baixo.className = "material-icons down"
+            baixo.innerHTML = "expand_more"
         var cima = document.createElement("span");
-        cima.className = "material-icons up"
-        cima.innerHTML = "expand_less"
+            cima.className = "material-icons up"
+            cima.innerHTML = "expand_less"
         var direita = document.createElement("div");
         $(direita).append(lapis,baixo,cima);
         $(linha).append(lixeira,campo,direita);
         $("ul").append(linha);
     }
-    $("button").on("click",function(){
+$(function () {
+    $("ul").on("click",".texto",function(){
+        $(this).toggleClass("sublinhado");
+        event.stopPropagation();
+    });
+    $("ul").on("click",".delete",function(event){
+        $(this).parent().remove();
+        event.stopPropagation();
+    });
+    $("ul").on("click",".up",function(event){
+        const $eleLista = ($(this).parent().parent());
+        $eleLista.prev().before($eleLista);
+        event.stopPropagation();
+    });
+    $("ul").on("click",".down",function(event){
+        const $eleLista = ($(this).parent().parent());
+        $eleLista.next().after($eleLista);
+        event.stopPropagation();
+    });
+    
+    $("button").on("click",function(event){
         criarTarefa();
+        event.stopPropagation();
     });
 });
