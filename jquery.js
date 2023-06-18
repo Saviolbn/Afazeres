8f8a14c25a534bd980241600bd7c8d85a903ce6f
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2023-06-18 11:36:30 -0300

    html orientado a classe

--- jquery.js
+++ jquery.js
@@ -1,58 +1,78 @@
-function criarTarefa(tarefa){
-        if (!tarefa.val()){return}
-        var linha = document.createElement("li");
-        var lixeira = document.createElement("span");
-            lixeira.className = "material-icons delete"
-            lixeira.innerHTML = "delete";
-        var campo = document.createElement("span");
-            campo.className = "texto";
-            campo.innerHTML = tarefa.val();
-        var lapis = document.createElement("span");
-            lapis.className = "material-icons edit"
-            lapis.innerHTML = "edit"
-        var baixo = document.createElement("span");
-            baixo.className = "material-icons down"
-            baixo.innerHTML = "expand_more"
-        var cima = document.createElement("span");
-            cima.className = "material-icons up"
-            cima.innerHTML = "expand_less"
-        $(linha).append(lixeira,campo,lapis,baixo,cima);
-        $("ul").append(linha);
-        tarefa.val("");
-    }
+function criarTarefa(tarefa) {
+    if (!tarefa.val()) { return }
+
+    const linha = document.createElement("li");
+    linha.className = "listItem"
+
+    const lixeira = document.createElement("span");
+    lixeira.className = "material-icons delete"
+    lixeira.innerText = "delete";
+
+    const campo = document.createElement("span");
+    campo.className = "texto";
+    campo.innerText = tarefa.val();
+
+    const lapis = document.createElement("span");
+    lapis.className = "material-icons edit"
+    lapis.innerText = "edit"
+
+    const baixo = document.createElement("span");
+    baixo.className = "material-icons down"
+    baixo.innerText = "expand_more"
+
+    const cima = document.createElement("span");
+    cima.className = "material-icons up"
+    cima.innerText = "expand_less"
+
+    $(linha).append(lixeira, campo, lapis, baixo, cima);
+    $(".lista").append(linha);
+    tarefa.val("");
+}
+
 $(function () {
-    $("ul").on("click","li",function(event){
+    //marcar como concluida a tarefa
+    $(".lista").on("click", "li", function (event) {
         $(this).find(".texto").toggleClass("sublinhado");
         event.stopPropagation();
     });
-    $("ul").on("click",".delete",function(event){
-        $(this).parent().fadeOut(function(){   
+
+    //Apagar tarefa
+    $(".lista").on("click", ".delete", function (event) {
+        $(this).parent().fadeOut(function () {
             $(this).remove();
         });
         event.stopPropagation();
     });
-    $("ul").on("click",".up",function(event){
-        const $eleLista = ($(this).parent());
-        $eleLista.prev().before($eleLista);
+
+    //Mover o elemento da lista para cima
+    $(".lista").on("click", ".up", function (event) {
+        const eleLista = $(this).parent();
+        eleLista.prev().before($eleLista);
         event.stopPropagation();
     });
-    $("ul").on("click",".down",function(event){
-        const $eleLista = ($(this).parent());
-        $eleLista.next().after($eleLista);
+
+    //Mover o elemento da lista para baxio
+    $(".lista").on("click", ".down", function (event) {
+        const eleLista = $(this).parent();
+        eleLista.next().after($eleLista);
         event.stopPropagation();
     });
-    $("form").on("submit",function(event){
-        var tarefa = $("#novaTarefa");
+
+    //Adicionar tarefa a lista
+    $(".listaFormulario").on("submit", function (event) {
+        const tarefa = $("#novaTarefa");
         criarTarefa(tarefa);
         event.stopPropagation();
     });
-    $("h1").on("click",".add",function (event) {
-        if ($("input").length && $("input").val().length) {
-            var tarefa = $("#novaTarefa").val();
-            criarTarefa(tarefa);
+
+    //Lida com a funcionalidade do botão "+"
+    $(".listaTitulo").on("click", ".add", function (event) {
+        const novoInput = $(".listaInput");
+        if (novoInput.length && novoInput.val().length) {
+            criarTarefa(novoInput);
             event.stopPropagation();
         } else {
-            $("input").fadeToggle(1000);
+            $(".listaFormulario").fadeToggle(1000);
         }
     })
 });
